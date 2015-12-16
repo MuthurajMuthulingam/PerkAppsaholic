9c47088f84c4c07299f2383e901c9201abf718df
Muthuraj Muthulingam <muthurajmuthulingam@gmail.com>
2015-12-16 08:02:04 +0530

    Initial Commit

---
+++ PerkAppsaholic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PerkAppsaholic
+//
+//  Created by Muthuraj Muthulingam on 12/16/15.
+//  Copyright © 2015 Muthuraj Muthulingam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PerkAppsaholic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PerkAppsaholic
+//
+//  Created by Muthuraj Muthulingam on 12/16/15.
+//  Copyright © 2015 Muthuraj Muthulingam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
